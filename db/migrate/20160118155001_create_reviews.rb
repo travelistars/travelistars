2b3768324407622313730163c745581508bb6b71
tomy.yat <tomy.yat@gmail.com>
2016-01-27 01:14:34 +0900

    added review field in review table

--- db/migrate/20160118155001_create_reviews.rb
+++ db/migrate/20160118155001_create_reviews.rb
@@ -2,9 +2,12 @@ class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
       t.references :review
+      t.references :entry
+      t.text :review, null: false
 
       t.timestamps
     end
     add_index :reviews, :review_id
+    add_index :reviews, :entry_id
   end
 end
